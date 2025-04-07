569a3ad38220d37fea1b430a5a8b96ce6bc02259
Lightnyng83 <159772828+Lightnyng83@users.noreply.github.com>
2025-04-07 15:53:40 +0200

    Add - Structure

---
+++ MicroFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MicroFrontend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
